eb4a9bead63728770bc3c09b74f58de61a2b96ca
杨明昭 <yangmingzhao@yangminhaodeMBP.lan>
2018-09-17 21:46:34 +0800

    分类

--- controllers/api.php
+++ controllers/api.php
@@ -10,7 +10,7 @@ class Api extends IController
     const HOST = 'http://119.23.239.216';
 
     private $result = ['code'=>200,'msg'=>'success','data'=>[]];
-	//擂台游戏 - 获取擂主
+	//banner - 获取banner
 	public function get_banner()
 	{
         $data = array_merge(IWeb::$app->config,$this->_siteConfig->getInfo(),array("form_index" => IFilter::act(IReq::get('form_index'))));
@@ -23,4 +23,16 @@ class Api extends IController
         $this->result['data']=$index_slide;
         echo json_encode($this->result);
 	}
+	//获取分类
+
+    public function get_category()
+    {
+        $catObj       = new IModel('category');
+        $catRow = $catObj->query('parent_id = 0','id,name,sort');
+        $this->result['data']= $catRow;
+        echo json_encode($this->result);
+    }
+
+
+
 }
