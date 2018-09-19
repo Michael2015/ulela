8ef23cf76b88ee650742280830647e13987927cf
yangmingzhao <yangmingzhao@diag.launch>
2018-09-19 18:09:14 +0800

    commit

--- controllers/api.php
+++ controllers/api.php
@@ -67,5 +67,33 @@ class Api extends IController
         echo json_encode($this->result);
     }
 
+    //获取商品详情
+    public function get_goods_detail()
+    {
+        $goods_id = IFilter::act(IReq::get('id'),'int');
+        if(!$goods_id)
+        {
+            $this->result['msg'] = 'fail';
+            $this->result['code'] = 0;
+            echo json_encode($this->result);exit;
+        }
+        //使用商品id获得商品信息
+        $tb_goods = new IModel('goods');
+        $goods_info = $tb_goods->getObj('id='.$goods_id." AND is_del=0");
+        $data = [];
+        if($goods_info)
+        {
+            $data['id'] = $goods_info['id'];
+            $data['sell_price'] = $goods_info['sell_price'];
+            $data['store_nums'] = $goods_info['store_nums'];
+            $data['img'] = $goods_info['img'];
+            $data['content'] = $goods_info['content'];
+            $data['description'] = $goods_info['description'];
+            $data['spec_array'] = $goods_info['spec_array'];
+        }
+        $this->result['data'] = $data;
+        echo json_encode($this->result);
+    }
+
 
 }
